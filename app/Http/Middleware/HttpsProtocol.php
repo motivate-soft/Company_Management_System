3c607eb7a88582741ddf299f1da663ba57b377b4
robert-upwork <robyop.eyeco@gmail.com>
2020-07-31 07:57:25 +0700

    Protocol

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+use Closure;
+
+
+class HttpsProtocol {
+
+
+
+    public function handle($request, Closure $next)
+
+    {
+
+            if (!$request->secure()) {
+
+                // return redirect()->secure($request->getRequestUri());
+
+            }
+
+
+
+            return $next($request); 
+
+    }
+
+}
+
