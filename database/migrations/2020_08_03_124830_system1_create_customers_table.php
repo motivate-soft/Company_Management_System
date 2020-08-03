4b523c674aa297b357f4224da2bed59aa22f5399
stepansavka <stepansavka@protonmail.com>
2020-08-03 20:11:39 +0700

    systemOne

--- database/migrations/2020_08_03_124830_system1_create_customers_table.php
+++ database/migrations/2020_08_03_124830_system1_create_customers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCustomersTable extends Migration
+class System1CreateCustomersTable extends Migration
 {
     /**
      * Run the migrations.
